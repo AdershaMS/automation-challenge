780302c1f68a61441f1ba8b69f8c0aa34a1e1d51
AdershaMS <49125485+AdershaMS@users.noreply.github.com>
2019-03-31 20:10:52 +0100

    Update login_page.rb
    Created the login page with page objects username, password and login button and passed the values. Validate the error message

--- features/pages/login_page.rb
+++ features/pages/login_page.rb
@@ -2,6 +2,29 @@ class LoginPage
 
   def initialize(browser)
     @browser = browser
+    @txtUsername = @browser.find_element(:id, "login_email")
+    @txtPassword = @browser.find_element(:id, "login_password")
+    @btnLogin = @browser.find_element(:name, "commit")
   end
 
+  def visit
+    @browser.get("https://login.sandbox.freeagent.com/")
+  end
+
+  def enterUsername(username)
+    @txtUsername.send_keys username
+  end
+
+  def enterPassword(password)
+    @txtPassword.send_keys password
+  end
+
+  def clickLoginButton
+    @btnLogin.click
+  end
+
+  def verifyFailure()
+    @browser.find_element(:class, "flash").exists()
+  end
+  
 end
