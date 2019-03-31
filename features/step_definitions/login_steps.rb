111ae890ef311bb4b1e42822c88f0ea59771632e
AdershaMS <49125485+AdershaMS@users.noreply.github.com>
2019-03-31 20:19:05 +0100

    Update login_steps.rb
    Added the step definitions

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,11 +1,14 @@
 Given /^I navigate to the login page$/ do
-
+  @LoginPage = LoginPage.new(@browser)
+  @LoginPage.visit
 end
 
 When /^I login with an invalid credential combination$/ do
- 
+ @LoginPage.enterUsername("ABC@YAHOO.COM")
+ @LoginPage.enterPassword("yyyyyyy")
+ @LoginPage.clickLoginButton
 end
 
 Then /^I should see the invalid credentials login error message$/ do
- 
+ @LoginPage.verifyFailure
 end
